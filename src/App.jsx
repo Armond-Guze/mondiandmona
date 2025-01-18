32361ed3583a604413f0614dd3712a53b0162e0d
Armond <mondi@Armonds-Laptop.local>
2025-01-18 00:48:58 -0500

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div><Navbar/></div>
-    //hello
-  )
-}
+    <div>
+      <Navbar />
+      <Header />
+    </div>
+  );
+};
 
-export default App
+export default App;
